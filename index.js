6203f9d3592a26d0266b4fced4d4309f6eb25b06
Lee Edwards <ledwards@matthewrdssmbp2.attlocal.net>
2014-09-03 17:27:42 -0400

    Switch to multipart processing

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+  , app = module.exports = express()
+  , formidable = require('formidable')
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 })
 
-app.post('/trips', function(request, response) {
-	console.log(request);
-	var parsedBody = JSON.parse(request);
-
-	console.log(parsedBody.from);
-	console.log(parsedBody.headers['Subject']);
-	console.log(parsedBody.plain);
-	console.log(parsedBody.html);
-	console.log(parsedBody.reply_plain);
-
-	response.send('Message Received. Thanks!');
+app.post('/trips', function(req, res) {
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, files) {
+	  console.log(fields);
+	  res.writeHead(200, {'content-type': 'text/plain'});
+	  res.end('Message Received. Thanks!\r\n');
+	})
 })
