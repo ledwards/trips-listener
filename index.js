bb03e0689576154921cbd8586d864761dce04682
Lee Edwards <ledwards@matthewrdssmbp2.attlocal.net>
2014-09-03 17:04:09 -0400

    Test cloudmail

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello World!');
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Node app is running at localhost:" + app.get('port'));
+})
+
+app.post('/trips', function(request, response) {
+	console.log(request);
+	var parsedBody = JSON.parse(request);
+
+	console.log(parsedBody.from);
+	console.log(parsedBody.headers['Subject']);
+	console.log(parsedBody.plain);
+	console.log(parsedBody.html);
+	console.log(parsedBody.reply_plain);
+
+	response.send('Message Received. Thanks!');
 })
